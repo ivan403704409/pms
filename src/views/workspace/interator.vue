e5927c9fddd86e5ad6d9385f7fb0087ed6a2c56c
ivan <403704409@qq.com>
2018-01-11 18:03:02 +0800

    change empty to placeholder

--- src/views/workspace/interator.vue
+++ src/views/workspace/interator.vue
@@ -16,11 +16,8 @@
         </template>
         <!-- 按列数循环 -->
         <div　class="f-flex-box flex-item auto-width flex-wrap align-strech" v-else>
-            <div class="col flex-item" v-for="(box, index) in config.children">
-                <template v-if="!box.length">
-                    <div>Empty</div>
-                </template>
-                <template v-for="(item, index2) in box" v-else>
+            <div class="col flex-item" :class="{ placeholder: !box.length }" v-for="(box, index) in config.children">
+                <template v-for="(item, index2) in box" v-if="box.length">
                     <!-- 如果有子集，则继续递归 -->
                     <interator :config="item" v-if="item.children"></interator>
                     <!-- 否则直接渲染当前组件 -->
@@ -31,6 +28,27 @@
     </template>
 </unit>
 </template>
+
+<style lang="scss" scoped>
+.col{
+    &.placeholder::after{
+        display: flex;
+        align-self: stretch;
+        align-items: center;
+        justify-content: center;
+        content: 'Empty';
+        color: #bababa;
+        width: 100%;
+        min-height: 5em;
+        background-color: rgba(144,145,153,0.125);
+        border: dashed 1px rgba(144,145,153,0.5);
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        font-size: 0.8rem;
+        pointer-events: none;
+    }
+}
+</style>
 
 <script>
 import widgets from '@/widgets'
